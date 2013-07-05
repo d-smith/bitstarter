12a914a8749a80ed1ccf7a7509ec32735022968e
Ubuntu <ubuntu@ip-10-38-143-80.ec2.internal>
2013-07-05 23:19:21 +0000

    require fs

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 var buf = fs.readFileSync("./index.html");
 var msg = buf.toString();
 app.get('/', function(request, response) {
