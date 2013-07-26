ac9e882d1fdd6b63b6a113ffe175827fc3202cf1
d-smith <doug.smith.mail@gmail.com>
2013-07-25 21:50:59 -0600

    updates to show editor image

--- web.js
+++ web.js
@@ -18,6 +18,12 @@ app.get('/aws-process-execution.png', function(request,response) {
   response.send(aws_img);
 });
 
+var editor_img = fs.readFileSync("./Editor.png");
+app.get('/Editor.png', function(request,response) {
+  response.send(editor_img);
+});
+
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
